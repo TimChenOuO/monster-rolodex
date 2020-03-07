c3c4619606c1b0f385cb646e05f6a23bd0686ce9
Tim Chen <timchen@Tims-MacBook-Pro.local>
2020-03-07 16:11:31 +0800

    Updates

---
+++ precache-manifest.2c0fd8124dd4da9fd91c909aaefefa48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf85981fcaf68ef9f75fd14ce5d018ff",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c5f0c1605eab1f44ef8d",
+    "url": "/monster-rolodex/static/css/main.798467e4.chunk.css"
+  },
+  {
+    "revision": "2eeba163add5b87c789c",
+    "url": "/monster-rolodex/static/js/2.580e123d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.580e123d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f0c1605eab1f44ef8d",
+    "url": "/monster-rolodex/static/js/main.c0966770.chunk.js"
+  },
+  {
+    "revision": "63efd5b4df30228bd62a",
+    "url": "/monster-rolodex/static/js/runtime-main.0c638f9f.js"
+  }
+]);
